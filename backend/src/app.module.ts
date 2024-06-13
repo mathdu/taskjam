4a0d32a9ef6fad8435af2548f7d5babba4048c6c
Mathieu Duchesneau <mathieu@doubledash.io>
2024-06-13 19:40:08 +0100

    add mongoose in package.json, app.module, yarn.lock

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProjectModule } from './project/project.module';
 
 @Module({
-  imports: [ProjectModule],
+  imports: [ProjectModule, MongooseModule.forRoot('mongodb://mongo/nest')],
   controllers: [AppController],
   providers: [AppService],
 })
