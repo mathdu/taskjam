53ef70c6012ae3461a0920e7c70aa46f67ac0bed
Mathieu Duchesneau <mathieu@doubledash.io>
2024-06-16 16:19:51 +0100

    format displayed date in taskList

--- frontend/src/components/Tasks/taskList.tsx
+++ frontend/src/components/Tasks/taskList.tsx
@@ -1,4 +1,4 @@
-import { List, ReferenceField, SimpleList } from 'react-admin';
+import { DateField, List, ReferenceField, SimpleList } from 'react-admin';
 
 export const TaskList = () => (
   <List>
@@ -6,7 +6,7 @@ export const TaskList = () => (
       linkType="show"
       primaryText={(record) => record.title}
       secondaryText={() => <ReferenceField source="project" reference="projects" link="show" />}
-      tertiaryText={(record) => record.completedAt}
+      tertiaryText={() => <DateField source="completedAt" required={false} />}
     />
   </List>
 );
